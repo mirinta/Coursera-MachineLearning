5000a0f89e98e874d5c636c623831b4059af4ba5
Mirinta <mirinta@163.com>
2023-02-02 22:21:14 +0800

    ex1

---
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -0,0 +1,36 @@
+function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCostMulti) and gradient here.
+    %
+
+predictions = X * theta; % m by 1 vector, predictios of hypothesis over all training examples    
+    
+errors = predictions - y; % m by 1 vector, difference between predictions and desired outputs
+
+partials = X' * errors / m; % n+1 by 1 vector, partial derivatives
+
+theta = theta - alpha * partials; % update theta
+
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCostMulti(X, y, theta);
+
+end
+
+end
